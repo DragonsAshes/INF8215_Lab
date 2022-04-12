7e03007287299e3508e562a125145745b3f76ce7
DragonsAshes <seb.foucher15@gmail.com>
2022-04-12 16:24:42 +0200

    update threat script

--- lab3/threat.py
+++ lab3/threat.py
@@ -5,13 +5,17 @@ df = pd.read_csv('./data/beans_train.csv')
 keys = ['SIRA','HOROZ','DERMASON','BARBUNYA','CALI','BOMBAY','SEKER']
 dic = {key: None for key in keys}
 
-print(dic)
-
 for key in dic:
     dic[key] = df.loc[df['class'] == key, df.columns != 'class']
 
+# print stats for each column class by class
 for key in dic:
     print('\n','='*10, key, '='*10)
     for column in dic[key]:
         print(column, dic[key][column].max(), dic[key][column].min(), dic[key][column].mean())
 
+# print stats for class column by column
+for column in dic['SIRA']:
+    print('\n','='*10,column, '='*10)
+    for key in dic:
+        print(key, dic[key][column].max(), dic[key][column].min(), dic[key][column].mean())
