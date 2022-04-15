116083aeff300c8dd0226d2b590dd439b3d264f8
DragonsAshes <seb.foucher15@gmail.com>
2022-04-15 21:58:35 +0200

    push everything

--- lab3/my_bean_tester.py
+++ lab3/my_bean_tester.py
@@ -19,17 +19,40 @@ from sklearn.neighbors import *
 from sklearn import tree
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingRegressor
+
+
+parameters = {
+    "n_estimators":[5,10,50,100,250],
+    "max_depth":[2,4,8,16,32,None]
+    
+}
+def display(results):
+    print(f'Best parameters are: {results.best_params_}')
+    print("\n")
+    mean_score = results.cv_results_['mean_test_score']
+    std_score = results.cv_results_['std_test_score']
+    params = results.cv_results_['params']
+    for mean,std,params in zip(mean_score,std_score,params):
+        print(f'{round(mean,3)} + or -{round(std,3)} for the {params}')
 
 class MyBeanTester(BeanTester):
     def __init__(self):
         #self.randomforest = RandomForestClassifier(verbose=3, n_jobs=-1, max_depth=10, criterion='entropy')
-        self.randomforest = RandomForestClassifier(verbose=3, n_jobs=-1, criterion='entropy', random_state=0)
+        self.randomforest = RandomForestClassifier(verbose=3, n_jobs=-1, criterion='entropy')
+        #self.randomforest = ExtraTreesClassifier(min_samples_split=2)
+        #self.randomforest = GradientBoostingClassifier(n_estimators=100, learning_rate=1.0, max_depth=1, random_state=0)
+        #self.randomforest = GridSearchCV(RandomForestClassifier(),parameters,cv=5)
         #self.randomforest = KNeighborsClassifier()
         #self.randomforest = linear_model.LogisticRegression(C=1e5)
         #self.randomforest = svm.SVC(kernel='linear')
         #self.randomforest = tree.DecisionTreeClassifier()
         #self.pca = PCA()
-        self.pca = PCA(n_components=13)
+        self.pca = PCA(n_components=12)
         self.sc = StandardScaler()
 
     def train(self, X_train, y_train):
@@ -59,12 +82,12 @@ class MyBeanTester(BeanTester):
         X_train = self.sc.fit_transform(X_train)
         X_train = self.pca.fit_transform(X_train)
 
-        print(X_train[0])
-
-        print(self.pca.explained_variance_ratio_)
+        #X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.4, random_state=0)
             
         #X_train = np.delete(X_train[:,1:], [2,3], 1)
         self.randomforest.fit(X_train, y_train)
+        #print("score : ",a.score(X_test, y_test))
+        #display(self.randomforest)
 
 
     def predict(self, X_data):
