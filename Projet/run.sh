a2923e78a8d618f2529631023ad6292baf0c8e8e
DragonsAshes <seb.foucher15@gmail.com>
2022-04-21 23:25:45 +0200

    add screen for hack

--- Projet/run.sh
+++ Projet/run.sh
@@ -10,13 +10,13 @@ function finish() {
 	kill -9 $PID2
 }
 
-python greedy_player.py -b localhost -p $PORT1 &
+python my_player.py -b localhost -p $PORT1 &
 PID1=$!
-#python good_player.py -b localhost -p $PORT2 &
-#PID2=$!
-#python game.py http://localhost:$PORT2 http://localhost:$PORT1 --time 3000
+python greedy_player.py -b localhost -p $PORT2 &
+PID2=$!
+python game.py http://localhost:$PORT2 http://localhost:$PORT1 --time 3000
 #python game.py http://localhost:$PORT1 human --time 3000
-python game.py human human --time 3000
+#python game.py human human --time 3000
 #python game.py human http://localhost:$PORT1 --time 3000
 
 finish
